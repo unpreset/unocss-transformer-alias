89cf43f53e136e3cd67aac0fd975f1c854d0ca3f
Chris <1633711653@qq.com>
2023-05-02 17:06:53 +0800

    feat: support nest shortcuts (#1)
    * feat: support nest shortcuts
    
    * chore: update
    
    * chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { expandVariantGroup, isStaticShortcut, isString } from '@unocss/core'
-import type { SourceCodeTransformer, UnoGenerator } from '@unocss/core'
+import type { ShortcutValue, SourceCodeTransformer, UnoGenerator } from '@unocss/core'
 import type MagicString from 'magic-string'
 
 export interface TransformerAliasOptions {
@@ -21,26 +21,58 @@ export default function transformerAlias(options: TransformerAliasOptions = {}):
   }
 }
 
-export function transformAlias(code: MagicString, uno: UnoGenerator, options: TransformerAliasOptions = {}) {
+export async function transformAlias(
+  code: MagicString,
+  uno: UnoGenerator,
+  options: TransformerAliasOptions = {},
+) {
   const prefix = options.prefix ?? '*'
   const extraRE = new RegExp(`${escapeRegExp(prefix)}([\\w-]+)`, 'g')
 
   for (const item of Array.from(code.original.matchAll(extraRE))) {
-    for (const shortcut of uno.config.shortcuts) {
-      let result
-      if (isStaticShortcut(shortcut)) {
-        if (shortcut[0] === item[1])
-          result = shortcut[1]
-      }
-      else {
-        const match = item[1].match(shortcut[0])
-        if (match !== null)
-          result = shortcut[1](match, uno.config as any)
-      }
-      if (isString(result))
-        code.overwrite(item.index!, item.index! + item[0].length, expandVariantGroup(result.trim()))
+    const result = await expandShortcut(item[1], uno)
+    if (!result)
+      continue
+
+    code.overwrite(item.index!, item.index! + item[0].length, result[0].join(' '))
+  }
+}
+
+export async function expandShortcut(
+  input: string,
+  uno: UnoGenerator,
+  depth = 5,
+): Promise<[ShortcutValue[]] | undefined> {
+  if (depth <= 0)
+    return
+
+  let result: string | ShortcutValue[] | undefined
+
+  for (const shortcut of uno.config.shortcuts) {
+    if (isStaticShortcut(shortcut)) {
+      if (shortcut[0] === input)
+        result = shortcut[1]
+    }
+    else {
+      const match = input.match(shortcut[0])
+      if (match != null)
+        result = shortcut[1](match, {} as any)
     }
   }
+
+  if (isString(result))
+    result = expandVariantGroup(result.trim()).split(/\s+/g)
+
+  if (!result)
+    return
+
+  return [
+    (await Promise.all(
+      result
+        .filter(s => s !== input)
+        .map(async r => (isString(r) ? (await expandShortcut(r, uno, depth - 1))?.[0] : undefined) || [r]),
+    )).flat(1).filter(Boolean),
+  ]
 }
 
 function escapeRegExp(str: string): string {
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,13 @@ import type { UnoGenerator } from '@unocss/core'
 import { createGenerator, expandVariantGroup } from '@unocss/core'
 import { describe, expect, test } from 'vitest'
 import MagicString from 'magic-string'
-import { transformAlias } from '../src'
+import { expandShortcut, transformAlias } from '../src'
 
 const uno = createGenerator({
   shortcuts: [
     ['btn', 'px-2 py-3 bg-blue-500 text-(white xl) rounded'],
-    [/^btn-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
-    [/^text-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
+    [/^btn-(.*)$/, ([, c]) => `btn bg-${c}4:10 text-${c}5`],
+    [/^primary-(.*)$/, ([, c]) => `border-${c} btn-${c} flex items-center`],
   ],
 })
 
@@ -23,13 +23,13 @@ function createTransformer(prefix = '*') {
 }
 
 describe('transformer alias', () => {
-  test('basic', () => {
+  test('basic', async () => {
     const transform = createTransformer()
 
     const code = `
 <template>
   <div *btn>
-    <div text-xl class="*text-red" />
+    <div text-xl class="*btn-red" />
     <div *btn-red />
   </div>
 </template>
@@ -37,21 +37,21 @@ describe('transformer alias', () => {
 
     expect(transform(code)).toMatchInlineSnapshot(`
       "<template>
-        <div px-2 py-3 bg-blue-500 text-white text-xl rounded>
-          <div text-xl class=\\"bg-red4:10 text-red5 rounded\\" />
-          <div bg-red4:10 text-red5 rounded />
+        <div *btn>
+          <div text-xl class=\\"*btn-red\\" />
+          <div *btn-red />
         </div>
       </template>"
     `)
   })
 
-  test('prefix', () => {
+  test('prefix', async () => {
     const transform = createTransformer('&')
 
     const code = `
 <template>
-  <div &btn>
-    <div text-xl class="&text-red" />
+  <div &test>
+    <div text-xl class="&primary-red" />
     <div *btn-red />
   </div>
 </template>
@@ -59,13 +59,49 @@ describe('transformer alias', () => {
 
     expect(transform(code)).toMatchInlineSnapshot(`
       "<template>
-        <div px-2 py-3 bg-blue-500 text-white text-xl rounded>
-          <div text-xl class=\\"bg-red4:10 text-red5 rounded\\" />
+        <div &test>
+          <div text-xl class=\\"&primary-red\\" />
           <div *btn-red />
         </div>
       </template>"
     `)
+  })
 
-    expect(expandVariantGroup('bg-blue-500 text-(white xl) rounded')).toMatchInlineSnapshot('"bg-blue-500 text-white text-xl rounded"')
+  test('expand shortcut', async () => {
+    const code = `
+    <template>
+        <div class="*btn-red" />
+        <div *btn-red />
+    </template>
+        `.trim()
+    const transform = createTransformer()
+
+    expect(transform(code)).toMatchInlineSnapshot(`
+      "<template>
+              <div class=\\"*btn-red\\" />
+              <div *btn-red />
+          </template>"
+    `)
+
+    expect(expandVariantGroup('text-(white xl) rounded')).toBe('text-white text-xl rounded')
+
+    expect(await expandShortcut('primary-red', uno))
+      .toMatchInlineSnapshot(`
+        [
+          [
+            "border-red",
+            "px-2",
+            "py-3",
+            "bg-blue-500",
+            "text-white",
+            "text-xl",
+            "rounded",
+            "bg-red4:10",
+            "text-red5",
+            "flex",
+            "items-center",
+          ],
+        ]
+      `)
   })
 })
